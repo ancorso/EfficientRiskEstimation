2fa431a88e54090e28080395391b76b09e4b599b
Anthony Corso <anthonycorso92@gmail.com>
2021-12-15 17:48:29 -0800

    added discrete pendulum

--- demo/continuous_pendulum.jl
+++ demo/continuous_pendulum.jl
@@ -10,21 +10,13 @@ policy = ActorCritic(GaussianPolicy(ContinuousNetwork(Chain(Dense(3, 32, relu), 
 policy = solve(PPO(π=policy, S=state_space(mdp), N=20000, ΔN=400), mdp)
 
 # Construct the adversarial MDP to get access to a transition function like gen(mdp, s, a, x)
-px = Normal(0f0, 0.5f0)
+px = Normal(0f0, 2f0)
 amdp = AdditiveAdversarialMDP(mdp, px)
 
 # Construct the risk estimation mdp where actions are disturbances
 rmdp = RMDP(amdp, policy, (m, s) -> 1 / abs(s[1] - mdp.failure_thresh))
 
-rmdp.cost_fn(mdp, [3,4,5])
-
-reward(rmdp, rand(initialstate(rmdp)))
-
-
 samps = [maximum(collect(simulate(HistoryRecorder(), rmdp, FunctionPolicy((s) -> rand(px)))[:r])) for _ in 1:10000]
-
-histogram(samps)
-
 
 # Construct the MDP
 
---
+++ demo/discrete_pendulum.jl
@@ -0,0 +1,29 @@
+using POMDPs, POMDPGym, POMDPSimulators, POMDPPolicies, Distributions, Plots
+using Crux, Flux
+
+# Basic MDP
+mdp = InvertedPendulumMDP(λcost=0, include_time_in_state=true)
+
+# Learn a policy that solves it
+policy = ActorCritic(GaussianPolicy(ContinuousNetwork(Chain(Dense(3, 32, relu), Dense(32, 1))), [0f0]), 
+                     ContinuousNetwork(Chain(Dense(3, 32, relu), Dense(32, 1))))
+policy = solve(PPO(π=policy, S=state_space(mdp), N=20000, ΔN=400), mdp)
+
+
+# Define the disturbance distribution based on a normal distribution
+xnom = Normal(0f0, 0.5f0)
+xs = Float32[-2., -0.5, 0, 0.5, 2.]
+ps = exp.([logpdf(xnom, x) for x in xs])
+ps ./= sum(ps)
+px = DiscreteNonParametric(xs, ps)
+
+# Construct the adversarial MDP to get access to a transition function like gen(mdp, s, a, x)
+amdp = AdditiveAdversarialMDP(mdp, px)
+
+# Construct the risk estimation mdp where actions are disturbances
+rmdp = RMDP(amdp, policy, (m, s) -> 1 / (abs(s[1] - mdp.failure_thresh) + 1e-3))
+
+samps = [maximum(collect(simulate(HistoryRecorder(), rmdp, FunctionPolicy((s) -> rand(px)))[:r])) for _ in 1:10000]
+
+histogram(samps)
+

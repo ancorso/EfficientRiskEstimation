9fdbcb95f4962512dc2972f0ee1410a3c97d95a0
Anthony Corso <anthonycorso92@gmail.com>
2022-01-19 08:47:02 -0800

    added updates to pendulum system

--- demo/discrete_pendulum.jl
+++ demo/discrete_pendulum.jl
@@ -1,29 +1,44 @@
 using POMDPs, POMDPGym, POMDPSimulators, POMDPPolicies, Distributions, Plots
 using Crux, Flux
+using BSON
+using ImportanceWeightedRiskMetrics
 
-# Basic MDP
-mdp = InvertedPendulumMDP(λcost=0, include_time_in_state=true)
+## Setup and solve the mdp
+mdp = InvertedPendulumMDP(λcost=1, Rstep=.1, dt=dt, px=Normal(0f0, 0.1f0))
+# policy = ActorCritic(GaussianPolicy(ContinuousNetwork(Chain(Dense(2, 32, relu), Dense(32, 1, tanh), x-> x .* 2f0), 1), [0f0]), 
+#                      ContinuousNetwork(Chain(Dense(2, 32, relu), Dense(32, 1))))
+# policy = solve(PPO(π=policy, S=state_space(mdp), N=100000, ΔN=400, max_steps=400), mdp)
+# BSON.@save "policies/pendulum_policy.bson" policy
 
-# Learn a policy that solves it
-policy = ActorCritic(GaussianPolicy(ContinuousNetwork(Chain(Dense(3, 32, relu), Dense(32, 1))), [0f0]), 
-                     ContinuousNetwork(Chain(Dense(3, 32, relu), Dense(32, 1))))
-policy = solve(PPO(π=policy, S=state_space(mdp), N=20000, ΔN=400), mdp)
+policy = BSON.load("policies/pendulum_policy.bson")[:policy]
+Crux.gif(mdp, policy, "out.gif", max_steps=100,)
 
 
-# Define the disturbance distribution based on a normal distribution
-xnom = Normal(0f0, 0.5f0)
-xs = Float32[-2., -0.5, 0, 0.5, 2.]
-ps = exp.([logpdf(xnom, x) for x in xs])
-ps ./= sum(ps)
-px = DiscreteNonParametric(xs, ps)
+## Construct the risk estimation mdp where actions are disturbances
+dt = 0.1
+maxT = 2.0
+# px = Normal(0f0, 0.35f0)
+px = Normal(0f0, 1.0f0)
 
-# Construct the adversarial MDP to get access to a transition function like gen(mdp, s, a, x)
-amdp = AdditiveAdversarialMDP(mdp, px)
+# cost environment
+env = InvertedPendulumMDP(dt=dt, failure_thresh=Inf)
+costfn(m, s, sp) = isterminal(m, sp) ? abs(sp[2]) : 0
+rmdp = RMDP(env, policy, costfn, true, dt, maxT)
 
-# Construct the risk estimation mdp where actions are disturbances
-rmdp = RMDP(amdp, policy, (m, s) -> 1 / (abs(s[1] - mdp.failure_thresh) + 1e-3))
+# Failure environment
+env = InvertedPendulumMDP(dt=dt, failure_thresh=π/2)
+costfn(m, s, sp) = isterminal(m, sp) ? isfailure(m, sp) : 0
+rmdp = RMDP(env, policy, costfn, true, dt, maxT)
 
-samps = [maximum(collect(simulate(HistoryRecorder(), rmdp, FunctionPolicy((s) -> rand(px)))[:r])) for _ in 1:10000]
 
-histogram(samps)
+samps = [simulate(RolloutSimulator(), rmdp, FunctionPolicy((s) -> rand(px))) for _=1:10000]
+
+m = IWRiskMetrics(samps, ones(length(samps)), 0.01)
+
+
+histogram(samps, alpha=0.5, label="MC")
+vline!([m.mean], label="mean", color=1)
+vline!([m.var], label="var", linestyle=:dashdot, color=1)
+vline!([m.cvar], label="cvar", linestyle=:dash, color=1)
+vline!([m.worst], label="worst case", linestyle=:dot, color=1)
 
